c591df81945a992fae6a6402fba44091924074f4
Prajualit Tickoo <23bec072@nith.ac.in>
2025-05-20 00:35:12 +0530

    fixes

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,8 +1,8 @@
-import apiResponse from "../utils/apiResponse.js";
-import apiError from "../utils/apiError.js";
-import asyncHandler from "../utils/asyncHandler.js";
+import { apiResponse } from "../utils/apiResponse.js";
+import { apiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
-import User from "../models/user.model.js";
+import { User } from "../models/user.model.js";
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
