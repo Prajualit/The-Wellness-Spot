059dd02c62fcf6ec2de2eb8bd2ca6733b556fc9a
Prajualit Tickoo <23bec072@nith.ac.in>
2025-05-20 18:15:12 +0530

    otp setup

---
+++ frontend/src/app/login/page.jsx
@@ -0,0 +1,9 @@
+import OtpAuth from '@/components/OtpAuth';
+
+export default function page() {
+    return (
+        <main className="flex justify-center items-center min-h-screen">
+            <OtpAuth />
+        </main>
+    );
+}
