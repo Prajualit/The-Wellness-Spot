4cba73131bf3a502e98b383fb4b50a3e19690cc5
Prajualit Tickoo <23bec072@nith.ac.in>
2025-05-27 23:49:39 +0530

    axios setup

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,12 @@
+// lib/axios.js
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000",
+  withCredentials: true, // ensures cookies (like accessToken) are sent
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default instance;
