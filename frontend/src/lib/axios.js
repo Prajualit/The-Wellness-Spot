31d7c29d4d33efbf142f85248e50a6570487c46d
Prajualit Tickoo <23bec072@nith.ac.in>
2025-06-01 02:29:36 +0530

    axios fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,11 +2,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000/api/v1",
-  withCredentials: true, // ensures cookies (like accessToken) are sent
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL:
+    process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000/api/v1",
+  withCredentials: true,
 });
 
 export default instance;
