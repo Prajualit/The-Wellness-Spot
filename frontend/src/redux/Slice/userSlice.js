bc8ed6621aeb61791243b3650fc053f8c742fab6
Prajualit Tickoo <23bec072@nith.ac.in>
2025-06-01 01:26:14 +0530

    redux fix

--- frontend/src/redux/Slice/userSlice.js
+++ frontend/src/redux/Slice/userSlice.js
@@ -12,11 +12,27 @@ const userSlice = createSlice({
     setUser(state, action) {
       state.user = action.payload;
     },
+    updateUser(state, action) {
+      if (!state.user) return;
+
+      if (action.payload.media) {
+        if (!Array.isArray(state.user.media)) {
+          state.user.media = [];
+        }
+
+        state.user.media.push(action.payload.media);
+      }
+
+      state.user = {
+        ...state.user,
+        ...action.payload,
+      };
+    },
     clearUser(state) {
       state.user = null;
     },
   },
 });
 
-export const { setUser, clearUser } = userSlice.actions;
+export const { setUser, clearUser, updateUser } = userSlice.actions;
 export default userSlice.reducer;
