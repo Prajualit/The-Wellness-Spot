8285ee9037f6043c066eed6cb6ee3df883034a04
Prajualit Tickoo <23bec072@nith.ac.in>
2025-06-01 01:28:05 +0530

    route fixes

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,13 @@
 import { loginUser, logoutUser } from "../controllers/user.controller.js";
+import { updateAvatar } from "../controllers/updateAvatar.controller.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
+import { upload } from "../middleware/multer.middleware.js";
 import { Router } from "express";
 
 const router = Router();
 
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/update-avatar").post(verifyJWT, upload.single("file"), updateAvatar);
 
 export default router;
