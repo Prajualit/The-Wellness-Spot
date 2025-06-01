4e4dda39b00d3331d85f45150b747c3fa08787f4
Prajualit Tickoo <23bec072@nith.ac.in>
2025-06-01 23:59:47 +0530

    data populated in table

--- frontend/src/components/dashboard/table.jsx
+++ frontend/src/components/dashboard/table.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { Button } from '../ui/button';
 
 const Table = () => {
 
     const Thead = [
-        'Date',
-        'Blood Sugar',
-        'Blood Pressure',
         'BMI',
         'Weight',
+        'Height',
         'Actions'
     ];
+
+    const user = useSelector((state) => state.user.user);
 
     return (
         <table className="flex-1">
@@ -27,26 +29,26 @@ const Table = () => {
                 </tr>
             </thead>
             <tbody>
-                <tr className="border-t border-t-[#d4dde2]">
-                    <td className="table-952c22f1-0138-435f-b64e-7ec1d8771011-column-120 h-[72px] px-4 py-2 w-[400px] text-[#5c778a] text-sm font-normal leading-normal">
-                        2024-07-20
-                    </td>
-                    <td className="table-952c22f1-0138-435f-b64e-7ec1d8771011-column-240 h-[72px] px-4 py-2 w-[400px] text-[#5c778a] text-sm font-normal leading-normal">
-                        95 mg/dL
-                    </td>
-                    <td className="table-952c22f1-0138-435f-b64e-7ec1d8771011-column-360 h-[72px] px-4 py-2 w-[400px] text-[#5c778a] text-sm font-normal leading-normal">
-                        120/80 mmHg
-                    </td>
-                    <td className="table-952c22f1-0138-435f-b64e-7ec1d8771011-column-480 h-[72px] px-4 py-2 w-[400px] text-[#5c778a] text-sm font-normal leading-normal">
-                        22.5
-                    </td>
-                    <td className="table-952c22f1-0138-435f-b64e-7ec1d8771011-column-600 h-[72px] px-4 py-2 w-[400px] text-[#5c778a] text-sm font-normal leading-normal">
-                        135 lbs
-                    </td>
-                    <td className="table-952c22f1-0138-435f-b64e-7ec1d8771011-column-720 h-[72px] px-4 py-2 w-60 text-[#5c778a] text-sm font-bold leading-normal tracking-[0.015em]">
-                        Delete
-                    </td>
-                </tr>
+                {user.records.map((item, index) => {
+                    return (
+                        <tr key={index} className="border-t border-t-[#d4dde2]">
+                            <td className="table-952c22f1-0138-435f-b64e-7ec1d8771011-column-240 h-[72px] px-4 py-2 w-[400px] text-[#5c778a] text-sm font-normal leading-normal">
+                                {item.weight}
+                            </td>
+                            <td className="table-952c22f1-0138-435f-b64e-7ec1d8771011-column-360 h-[72px] px-4 py-2 w-[400px] text-[#5c778a] text-sm font-normal leading-normal">
+                                {item.height}
+                            </td>
+                            <td className="table-952c22f1-0138-435f-b64e-7ec1d8771011-column-120 h-[72px] px-4 py-2 w-[400px] text-[#5c778a] text-sm font-normal leading-normal">
+                                {item.bmi}
+                            </td>
+                            <td className="table-952c22f1-0138-435f-b64e-7ec1d8771011-column-720 h-[72px] px-4 py-2 w-60 text-[#5c778a] text-sm font-bold leading-normal tracking-[0.015em]">
+                                <Button>
+                                    Delete
+                                </Button>
+                            </td>
+                        </tr>
+                    )
+                })}
             </tbody>
         </table>
     )
